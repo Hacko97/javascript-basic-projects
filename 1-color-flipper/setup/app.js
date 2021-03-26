b1ec40629293044f8d4fbaf4d4ac69f1cfeda855
Dines <42099788+Hacko97@users.noreply.github.com>
2021-03-26 06:50:50 +0530

    js

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -1,1 +1,17 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function() {
+    console.log(document.body);
+    //get random number between 0-3
+    const random = getRandomNumber();
+    document.body.style.backgroundColor = colors[random];
+    color.textContent = colors[random];
+
+
+});
+
+function getRandomNumber() {
+    return Math.floor((Math.random() * colors.length));
+}
